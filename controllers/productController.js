74cb4e71d539fcd7a3c9423c1c0c762e10bc631b
GabrielRuiseco <gabriel_alejandro_rc@outlook.com>
2022-02-20 21:26:06 -0600

    validator

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,21 +1,26 @@
 const Product = require('../models/Product');
+const { validationResult } = require('express-validator');
 
 exports.createProduct = async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const { name, price } = req.body;
   try {
-    console.log(req.body);
-    const { name, price } = req.body;
     let product = await Product.findOne({ where: { Name: name } });
     if (product) {
-      return res.status(400).json({ message: 'Producto existente' });
+      return res.status(400).json({ msg: 'Producto existente' });
     }
     product = {
       Name: name,
       Price: price,
     };
     await Product.create(product);
-    await res.status(200).json({ message: 'Producto agregado' });
+    await res.status(200).json({ msg: 'Producto agregado' });
   } catch (error) {
     console.log(error);
-    res.status(400).json({ message: 'Ha habido un error' });
+    res.status(400).json({ msg: 'Ha habido un error' });
   }
 };
--- routes/products.js
+++ routes/products.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const Product = require('../controllers/productController');
+const { check } = require('express-validator')
 
 //Products
 const router = express.Router();
 
-router.post('/', Product.createProduct);
+router.post('/', [
+  check('Name', 'El nombre es obligatorio').not().isEmpty(),
+  check('Price', 'El nombre es obligatorio').not().isEmpty()
+],Product.createProduct);
 
 
 module.exports = router;
