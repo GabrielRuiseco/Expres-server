bb186035e39b49528c7390fb4a5f256c6f4b53b4
GabrielRuiseco <gabriel_alejandro_rc@outlook.com>
2022-02-24 10:53:19 -0600

    Controller

--- controllers/productController.js
+++ controllers/productController.js
@@ -2,7 +2,7 @@ const Product = require('../models/Product');
 const { validationResult } = require('express-validator');
 
 exports.createProduct = async (req, res) => {
-  const errors = validationResult(req.body);
+  const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() });
   }
@@ -14,13 +14,66 @@ exports.createProduct = async (req, res) => {
       return res.status(400).json({ msg: 'Producto existente' });
     }
     product = {
-      Name: name,
       Price: price,
+      Name: name,
     };
     await Product.create(product);
     await res.status(200).json({ msg: 'Producto agregado' });
   } catch (error) {
     console.log(error);
     res.status(400).json({ msg: 'Ha habido un error' });
+  }
+};
+
+exports.getAllProducts = async (req, res) => {
+  try {
+    const products = await Product.findAll();
+    if (products.length <= 0) {
+      return res.status(404).json({ msg: 'Ningun resultado disponible' });
+    }
+    await res.status(200).json(products);
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ msg: 'Ha habido un error' });
+  }
+};
+
+exports.getProduct = async (req, res) => {
+  const { id } = req.body;
+  try {
+    const product = await Product.findByPk(id);
+    if (!product) {
+      return res.status(404).json({ msg: 'No encontrado' });
+    }
+    await res.status(200).json(product);
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ msg: 'Ha habido un error' });
+  }
+};
+
+exports.updateProduct = async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  const { id, name, price } = req.body;
+  try {
+    await Product.update({ Name: name, Price: price }, { where: { id: id } });
+    await res.status(200).json({ msg: 'Actualizado correctamente' });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ msg: 'Ha ocurrido un error' });
+  }
+};
+
+exports.deleteProduct = async (req, res) => {
+  const { id } = req.body;
+  try {
+    await Product.destroy({ where: { id: id } });
+    await res.status(200).json({ msg: 'Eliminado correctamente' });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ msg: 'Ha ocurrido un error' });
   }
 };
--- routes/products.js
+++ routes/products.js
@@ -1,14 +1,31 @@
 const express = require('express');
 const Product = require('../controllers/productController');
-const { check } = require('express-validator')
+const { check, body } = require('express-validator');
 
 //Products
 const router = express.Router();
 
-router.post('/', [
-  check('Name', 'El nombre es obligatorio').not().isEmpty(),
-  check('Price', 'El precio es obligatorio').not().isEmpty()
-],Product.createProduct);
+router.post(
+  '/',
+  [
+    body('name', 'El nombre es obligatorio').not().isEmpty(),
+    body('price', 'El precio es obligatorio').not().isEmpty(),
+  ],
+  Product.createProduct
+);
 
+router.get('/getOne', Product.getProduct);
+
+router.put(
+  '/update',
+  [
+    body('id', 'El id es requerido').not().isEmpty(),
+    body('name', 'El nombre es obligatorio').not().isEmpty(),
+    body('price', 'El precio es obligatorio').not().isEmpty(),
+  ],
+  Product.updateProduct
+);
+
+router.delete('/delete', Product.deleteProduct);
 
 module.exports = router;
